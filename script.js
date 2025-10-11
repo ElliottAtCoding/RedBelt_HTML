9081300d6fb6431fad61ded60970ad7e3105444b
egga22 <84204028+egga22@users.noreply.github.com>
2025-10-11 12:32:36 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+const Login = document.getElementById("LoginButton");
+Login.addEventListener("click", () => {
+  let loginusername = prompt("What is your username?");
+  let loginpassword = prompt("What is your password?");
+  const usernameToCheck = loginusername;
+  const apiKey = "68ea8f1c7f34ed3b0c200aaa";
+  let userExists = false;
+    fetch(
+        'https://hiscoretracker-67ep.restdb.io/rest/accounts?q={"username":"' + usernameToCheck + '"}', {
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "x-apikey": apiKey,
+                "cache-control": "no-cache",
+            },
+        }
+    )
+        .then((res) => {
+            if (!res.ok) throw new Error("Network response was not ok");
+            return res.json();
+        })
+        .then((data) => {
+            userExists = data.length > 0;
+            if (userExists) {
+                alert("user exists");
+            } else {
+                alert("User does not exist.");
+            }
+});
+const SignUp = document.getElementById("SignUpButton");
+SignUp.addEventListener("click", () => {
+  let signUpusername = prompt("What is your desired username?");
+  let signUppassword = prompt("What is your desired password?");
+  let userExists = false;
+    fetch(
+        'https://hiscoretracker-67ep.restdb.io/rest/accounts?q={"username":"' + usernameToCheck + '"}', {
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "x-apikey": apiKey,
+                "cache-control": "no-cache",
+            },
+        }
+    )
+        .then((res) => {
+            if (!res.ok) throw new Error("Network response was not ok");
+            return res.json();
+        })
+        .then((data) => {
+            userExists = data.length > 0;
+            if (userExists) {
+                console.log("User exists:", data);
+                // Here you would typically check the password as well
+            } else {
+                console.log("User does not exist.");
+            }
+});
+});
